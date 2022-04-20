359b480a8a237f6b54f0ccad8017987209c7e5d7
Kang-Dayeon <94333816+Kang-Dayeon@users.noreply.github.com>
2022-04-20 16:52:36 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -103,11 +103,8 @@ function saveClick(){
 
 if(canvas){
   canvas.addEventListener("mousemove", moveMouse);
-  canvas.addEventListener("touchmove", moveMouse);
   canvas.addEventListener("mousedown", startPainting);
-  canvas.addEventListener("touchend", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
-  canvas.addEventListener("touchstart", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("click", canvasClick);
   canvas.addEventListener("contextmenu", handleCTX);
